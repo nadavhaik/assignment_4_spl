a6b1e6dfd7fd2758bb9669d66465b65b91f795d6
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2022-01-11 22:40:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 
 from context_manager import ContextManager
 
+
 def main():
     print("Calling make -pizza..")
     cm = ContextManager(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
