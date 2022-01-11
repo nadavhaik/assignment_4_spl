d0a08e113ebc52774646b529862b0a4a1930332c
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2022-01-11 17:52:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from context_manager import ContextManager
 
 
 def main():
+    print("Calling makepizza..")
     cm = ContextManager(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     cm.run()
+    print("Done.")
 
 
 if __name__ == "__main__":
